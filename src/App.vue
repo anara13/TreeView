fc50f9140e145fef4d83e34b61f2d91809dd7254
anara_13 <you@example.com>
2022-06-17 11:48:43 +0200

    importation du projet dans le dépot

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <v-main>
+      <TreeView />      
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import TreeView from './components/TreeView';
+
+
+export default {
+  name: 'App',
+
+  components: {
+    TreeView,
+  },
+
+};
+</script>
---
+++ src/components/TreeView.vue
@@ -0,0 +1,96 @@
+<template>
+  <v-treeview
+    v-model="tree"
+    :open="initiallyOpen"
+    :items="items"
+    activatable
+    item-key="name"
+    open-on-click
+  >
+  <!--Rajouter ici le fichier -->
+    <template v-slot:prepend="{ item, open }">
+      <v-icon v-if="!item.file">
+        {{ open ? 'mdi-folder-open' : 'mdi-folder' }}
+      </v-icon>
+      <v-icon v-else>
+        {{ files[item.file] }}
+      </v-icon>
+    </template>
+  </v-treeview>
+</template> 
+
+<script>
+  export default {
+    props: {
+        fichierJSON: [Object, Array, JSON]
+    },
+    data: () => ({
+    //permet de se souvenir du dernier fichier ouvert
+      initiallyOpen: ['public'],
+      files: {
+        html: 'mdi-language-html5',
+        js: 'mdi-nodejs',
+        json: 'mdi-code-json',
+        md: 'mdi-language-markdown',
+        pdf: 'mdi-file-pdf',
+        png: 'mdi-file-image',
+        txt: 'mdi-file-document-outline',
+        xls: 'mdi-file-excel',
+      },
+      tree: [],
+      //faire passer ici en props les éléments du fichier parent (SideBar) 
+      items: [
+        {
+          name: '.git',
+        },
+        {
+          name: 'node_modules',
+        },
+        {
+          name: 'public',
+          children: [
+            {
+              name: 'static',
+              children: [{
+                name: 'logo.png',
+                file: 'png',
+              }],
+            },
+            {
+              name: 'favicon.ico',
+              file: 'png',
+            },
+            {
+              name: 'index.html',
+              file: 'html',
+            },
+          ],
+        },
+        {
+          name: '.gitignore',
+          file: 'txt',
+        },
+        {
+          name: 'babel.config.js',
+          file: 'js',
+        },
+        {
+          name: 'package.json',
+          file: 'json',
+        },
+        {
+          name: 'README.md',
+          file: 'md',
+        },
+        {
+          name: 'vue.config.js',
+          file: 'js',
+        },
+        {
+          name: 'yarn.lock',
+          file: 'txt',
+        },
+      ],
+    }),
+  }
+</script>
