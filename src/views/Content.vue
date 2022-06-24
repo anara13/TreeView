5b9898750cafde88d40871dd0377e0e9805f2222
anara_13 <you@example.com>
2022-06-24 16:51:22 +0200

    Modifications de la structure d'affichage des fichiers du TreeView dans le Content pas ok

--- src/views/Content.vue
+++ src/views/Content.vue
@@ -3,14 +3,14 @@
         <h2 class="h2">Gestion de documents</h2>
 
         <div class="main-content">
-            <audio v-if="pathExtension=='wav'" :src="pathItem" controls width="100%" height="auto" style="display:block; margin: 30vh auto" />
+            <!--<audio v-if="pathExtension=='wav'" :src="pathItem" controls width="100%" height="auto" style="display:block; margin: 30vh auto" />
             <video v-if="pathExtension=='avi' || pathExtension=='mp4' " :src="pathItem" controls width="100%" height="auto" /> 
-            <div v-if="pathExtension=='pdf' || pathExtension=='html' || pathExtension=='txt' " style="display:flex; justify-content:center;align-item:center;">
-                <iframe :src="pathItem" style="width: 100%; height:100vh" />
+            <div v-if="pathExtension=='pdf' || pathExtension=='html' || pathExtension=='txt'" style="display:flex; justify-content:center;align-item:center;">
+                <iframe :src="source" style="width: 100%; height:100vh" />
             </div>
             <div v-if="pathExtension=='jpg' || pathExtension=='png' || pathExtension=='gif' || pathExtension=='bmp' || pathExtension=='ico'">
                 <img :src="pathItem" style="display:block; margin: 30vh auto" />
-            </div>
+            </div>-->
             
             <!-- Rendering the loaded file -->
             <div v-if="resultLoaded" id="file-preview" style="width:100%;height:800px;">
@@ -20,7 +20,7 @@
                 <iframe v-else :src="source" style="width:99%;height:100%;" id="printf" name="printf"></iframe>
             </div>
 
-            <!--Waiting screen before server response-->
+            <!-- Waiting screen before server response-->
             <div v-if="calculatingResponse">
                 <svg class="rotator" style="width:120px;height:120px; margin: 130px auto 20px auto; display: block" viewBox="0 0 24 24">
                     <path fill="#ddd" d="M12,18A6,6 0 0,1 6,12C6,11 6.25,10.03 6.7,9.2L5.24,7.74C4.46,8.97 4,10.43 4,12A8,8 0 0,0 12,20V23L16,19L12,15M12,4V1L8,5L12,9V6A6,6 0 0,1 18,12C18,13 17.75,13.97 17.3,14.8L18.76,16.26C19.54,15.03 20,13.57 20,12A8,8 0 0,0 12,4Z" />
@@ -35,9 +35,9 @@
                 </div>
                 <h2 class="h2" style="text-align: center; color: #888;">Recherchez un fichier à afficher dans l'arborescence</h2>
             </div>
-        </div>
 
     </div>
+</div>
 </template>
 
 <script>
@@ -60,7 +60,7 @@
             this.$root.$on('item-left-clicked', itemPath => {
                 //this.resultLoaded = false;
                 //this.calculatingResponse = true;
-                this.pathItem = itemPath;
+                this.pathItem = itemPath.toString();
                 //this.pathExtension = itemPath.split('.').pop();
                 //this.$emit('clicked', 'true')
                 // TODO: sur l'exemple de https://stackoverflow.com/questions/27957766/how-do-i-render-a-word-document-doc-docx-in-the-browser-using-javascript
@@ -69,8 +69,7 @@
                 //this.fileType = "pdf";
                 
                 // const action = 'read';
-                // this.fetchAndReadFile(itemPath);
-                this.fetchAndPrintFile(itemPath);
+                this.fetchAndReadFile(this.pathItem);
             });                
 
             this.$root.$on('open-and-print-item', action => {
@@ -84,6 +83,7 @@
             this.addFontAwesome();
         }
 
+        //impossible de l'utiliser tant que pas intégré au projet principal
         /**
          * Récupère et affiche le fichier sélectionné 
          * TODO: intégrer un message d'erreur si le fichier n'a pas été trouvé (pour plus tard quand on aura accès au serveur)
@@ -108,29 +108,59 @@
             
         }*/
 
-        //TODO : à refaire
-        fetchAndPrintFile(itemPath) {
+        fetchAndPrintFile() {
             // TODO: "SecurityError: Permission denied to access property "print" on cross-origin object"
             // Erreur à cause de Firefox (aussi sur IE apparemment)
             // Check ici https://bugzilla.mozilla.org/show_bug.cgi?id=911444
             // iframe.src = "./fake_files/lorem_ipsum.pdf";
             // iframe.src = 'https://docs.google.com/gview?url='+'http://dictee.zenidoc.com:9000/cache/123456789.pdf';
-            //on crée ici un élément
-            console.log("entré fonction fetchAndPrintFile" + itemPath)
             let iframe = document.createElement('iframe');
             document.body.appendChild(iframe);
-            //TODO : ne marche plus, trouver un nouveau fichier test
-            //iframe.src = 'http://dictee.zenidoc.com:9000/cache/123456789.pdf';
-            iframe.src = '@/fake_files/' + itemPath;
-            //iframe.style.visibility = 'hidden';
+            iframe.src = 'http://dictee.zenidoc.com:9000/cache/123456789.pdf';
+            iframe.style.visibility = 'hidden';
             iframe.onload = function() {
                 console.log("file loaded");
                 setTimeout(function() {
                     iframe.contentWindow.focus();
-                    //iframe.contentWindow.print();
+                    iframe.contentWindow.print();
                     //iframe.contentWindow.document.getElementById('print').click();
                 }, 3000);
             };
+        }
+
+        fetchAndReadFile(itemPath){
+
+            //on récup le nom du fichier sélectionné
+            this.calculatingResponse = true;
+            //l'extension du fichier
+            this.pathExtension = itemPath.split('.').shift();
+            console.log(this.pathExtension)
+            //le nom du fichier
+            this.fileType = itemPath.split('.').pop();
+            console.log(this.fileType)
+
+            //le lien vers le fichier
+            //TODO : à des fins de test uniquement, à supprimer lorsque l'on pourra avoir accès aux fichiers
+            if(this.pathExtension == "pdf")
+            {
+                this.source = 'https://www.soundczech.cz/temp/lorem-ipsum.pdf';
+            }
+
+            else if(this.pathExtension == "mp4")
+            {
+                this.source = 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4';
+            }
+
+            else if(this.pathExtension == "png")
+            {
+                this.source = 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Google_Images_2015_logo.svg/640px-Google_Images_2015_logo.svg.png';
+            }
+
+            //indiquer au template que le fichier a bien été chargé
+            this.resultLoaded = true;
+
+            this.calculatingResponse = false;
+
         }
 
         /**
@@ -145,8 +175,8 @@
     }
 </script>
 
-<!--<style lang="stylus">
-    @import '../../main.styl';
+<style lang="stylus">
+    @import '../main.styl';
     .app > .content {
         width: calc(100% - 470px);
     }
@@ -164,7 +194,7 @@
         }
     }
 
-</style>-->
+</style>
 
 <style scoped>
 .main-content{
